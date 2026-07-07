a51c8d9908108337b5f37f17c7017fdfcdf66d2d
ashmitashrestha84 <ashmitashrestha247@gmail.com>
2026-07-07 22:53:09 +0545

    add:category controller

---
+++ src/controllers/category.controller.ts
@@ -0,0 +1,107 @@
+import { Request, Response, NextFunction } from "express";
+import Category from "../models/category.model";
+import { catchAsync } from "../utils/catchAsync.utils";
+import AppError from "../utils/appError.utils";
+
+
+export const getAll = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const categories = await Category.find();
+
+    res.status(200).json({
+      success: true,
+      count: categories.length,
+      data: categories,
+    });
+  },
+);
+
+export const getById = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    const category = await Category.findById(id);
+
+    if (!category) {
+      throw new AppError("category not found", 404);
+    }
+
+    res.status(200).json({
+      message: `category by id ${id} is fetched`,
+      success: true,
+      status: "success",
+      data: category,
+    });
+  },
+);
+
+
+export const create = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { name, description } = req.body;
+
+    if (!name) throw new AppError("name is required", 404);
+    if (!description) throw new AppError("description is required", 404);
+
+    const existingCategory = await Category.findOne({ name });
+    if (existingCategory) {
+      throw new AppError("name already exists", 404);
+    }
+
+    const category = new Category({ name, description });
+    await category.save();
+
+    res.status(201).json({
+      message: "Category created successfully",
+      status: "success",
+      success: true,
+      data: category,
+    });
+  },
+);
+
+
+
+export const update = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+
+    const existingCategory = await Category.findById({ id });
+
+    if (!existingCategory) {
+      throw new AppError("Brand does not exist", 404);
+    }
+
+    const { name, description } = req.body;
+
+    const updatedCategory = await Category.findByIdAndUpdate(
+      { _id: id },
+      { name, description },
+      { new: true },
+    );
+
+    res.status(201).json({
+      success: true,
+      message: "Brand updated successfully.",
+      data: updatedCategory,
+    });
+  },
+);
+
+
+export const remove = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { id } = req.params;
+    const category = await Category.findByIdAndDelete({ _id: id });
+
+    if (!category) {
+      throw new AppError("category not found.", 404);
+    }
+
+    res.status(200).json({
+      message: "category deleted successfully.",
+      success: true,
+      status: "success",
+      data: null,
+    });
+  },
+);
