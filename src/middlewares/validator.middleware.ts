16606dcc76ac5d3e9806822098566510a32b9126
ashmitashrestha84 <ashmitashrestha247@gmail.com>
2026-07-17 16:52:26 +0545

    add:structured

--- src/middlewares/validator.middleware.ts
+++ src/middlewares/validator.middleware.ts
@@ -1,36 +1,35 @@
 import { NextFunction, Request, Response } from "express";
-import {z} from "zod";
+import { z } from "zod";
 import appError from "../utils/appError.utils";
 
+export const validate = (schema: z.ZodObject) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.body);
+    const result = schema.safeParse({
+      body: req.body,
+      params: req.params,
+      query: req.query,
+    });
 
-export const validate=(schema:z.ZodObject)=>{
-    return (req:Request,res:Response,next:NextFunction)=>{
-        const result= schema.safeParse({
-            body:req.body,
-            params:req.params,
-            query:req.query,
-        });
+    //! if error
+    if (!result.success) {
+      const errors = result.error.issues.map(({ path, message }) => {
+        //array
+        return {
+          path: path.join("."),
+          message,
+        };
+      });
 
+      return next(new appError("validation error", 400, errors));
+    }
 
-        //! if error
-        if(!result.success){
-    
-            const errors=result.error.issues.map(({path, message})=>{   //array
-                return {
-                    path:path.join("."),
-                    message,
-                }
-                 })
-                 
-           return next(new appError("validation error",400,errors))
-        }
+    //* if validation passed
+    req.body = result.data.body;
+    req.params = result.data.params as Record<string, any>;
 
-        //* if validation passed
-        req.body=result.data.body;
-        req.params=  result.data.params as Record<string,any>;
+    Object.assign(req.query, result.data.query);
 
-        Object.assign(req.query,result.data.query);
-
-        next();
-    };
+    next();
+  };
 };
