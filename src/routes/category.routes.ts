9ba44d74d6a26219f57d112ee18a3ee6d0ee8267
ashmitashrestha84 <ashmitashrestha247@gmail.com>
2026-07-18 21:32:35 +0545

    Add:category validators

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -1,33 +1,27 @@
 import express from "express";
-import {
-  getAll,
-  getById,
-  create,
-  update,
-  remove,
-} from "../controllers/category.controller";
+import {getAll,getById,create,update,remove} from "../controllers/category.controller";
 import { uploader } from "../middlewares/multer.middleware";
 import { All_Admin} from "../types/enumtypes";
 import { authenticate } from "../middlewares/auth.middleware";
 import { validate } from "../middlewares/validator.middleware";
-import { categoryUserSchema } from "../validators/category. validator";
+import { categoryIdSchema, createCategorySchema, deleteCategorySchema, getCategoriesSchema, updateCategorySchema } from "../validators/category. validator";
 
 const router = express.Router();
 const upload=uploader();
 
 //* get all category
-router.get("/", getAll);
+router.get("/",validate(getCategoriesSchema),getAll);
 
 //* get by id
-router.get("/:id", getById);
+router.get("/:id",validate(categoryIdSchema),getById);
 
 //* create/post
-router.post("/",upload.single("logo"),authenticate(All_Admin),validate(categoryUserSchema),create);
+router.post("/",upload.single("logo"),authenticate(All_Admin),validate(createCategorySchema),create);
 
 //* update/put
-router.put("/:id",upload.single("logo"),authenticate(All_Admin),validate(categoryUserSchema),update);
+router.put("/:id",upload.single("logo"),authenticate(All_Admin),validate(updateCategorySchema),update);
 
 //* delete
-router.delete("/:id",authenticate(All_Admin),remove);
+router.delete("/:id",authenticate(All_Admin),validate(deleteCategorySchema),remove);
 
 export default router;
--- src/validators/category. validator.ts
+++ src/validators/category. validator.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export const categoryUserSchema = z.object({
+export const createCategorySchema = z.object({
   body: z.object({
     name: z
       .string("Name must be string")
@@ -13,5 +13,43 @@ export const categoryUserSchema = z.object({
       .trim()
       .min(10, "Description must be at least 10 characters long")
       .max(500, "Description cannot exceed 500 characters"),
+  }),
+});
+
+export const updateCategorySchema = z.object({
+  params: z.object({
+    id: z.string().min(1, "Category ID is required"),
+  }),
+
+  body: z.object({
+    name: z
+      .string()
+      .min(2, "Category name must be at least 2 characters")
+      .max(100, "Category name must not exceed 100 characters")
+      .optional(),
+
+    description: z
+      .string()
+      .min(5, "Description must be at least 5 characters")
+      .max(500, "Description must not exceed 500 characters")
+      .optional(),
+  }),
+});
+
+export const getCategoriesSchema = z.object({
+  query: z.object({
+    query: z.string().optional(),
+  }),
+});
+
+export const categoryIdSchema = z.object({
+  params: z.object({
+    id: z.string().min(1, "Category ID is required"),
+  }),
+});
+
+export const deleteCategorySchema = z.object({
+  params: z.object({
+    id: z.string().min(1, "Category ID is required"),
   }),
 });
